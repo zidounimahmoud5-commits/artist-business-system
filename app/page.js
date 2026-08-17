563ccfcf1b4f52bfd672b8a41b5da67f4f64ffc2
zidounimahmoud5-commits <zidounimahmoud5@gmail.com>
2026-08-17 11:14:06 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,14 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { supabase } from "../lib/supabaseClient";
+
+export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data }) => {
+      router.replace(data.session ? "/dashboard" : "/login");
+    });
+  }, [router]);
+  return null;
+}
